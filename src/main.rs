68a28e06e1dede380afbcf87b67867aac8ef1cef
Nathan <nslee333@gmail.com>
2025-11-17 19:58:44 -0800

    fibonacci sequence

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub fn fibonacci() {
+    let mut a = 0;
+    let mut b = 1;
+    let mut result = 0;
+
+    while result <= 1000 {
+        result = a + b;
+        a = b;
+        b = result;
+        println!("{result}");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    fibonacci();
 }
